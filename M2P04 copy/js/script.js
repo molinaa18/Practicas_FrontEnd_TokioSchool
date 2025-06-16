61783a694fc864209b3183f73349db1730c8a2d0
molinaa18 <javiermoga_97@hotmail.com>
2025-06-16 11:47:55 +0200

    #

---
+++ M2P04 copy/js/script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar-nav');
+  sidebar.classList.add('show');
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar-nav');
+  sidebar.classList.remove('show');
+}
